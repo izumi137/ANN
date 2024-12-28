c14617cb952bafd9058b64bb857c298e22af5285
chien <chien.phambach@gmail.com>
2024-12-28 08:40:06 +0700

    ref: rename

--- CPU/crossentropy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-using namespace std;
-class CE
-{
-public:
-    float crossEntropyLoss(const vector<vector<float>>& predictions, const vector<vector<int>>& labels);
-};
-
